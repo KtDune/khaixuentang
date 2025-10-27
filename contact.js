6a5e841d8d55e285bff7843131c43b0889dbd1cc
Khai Xuen Tang <angryxuen@gmail.com>
2025-10-27 13:05:24 +0000

    Fixed layout issue

--- contact.js
+++ contact.js
@@ -1,12 +1,23 @@
-document.getElementById('gmail-btn').addEventListener('click', function (event) {
-    event.preventDefault(); // prevent the link from opening mail app
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('gmail-btn').addEventListener('click', function (event) {
+        event.preventDefault(); // prevent the link from opening mail app
 
-    const email = 'khaixuen.tang@.com';
-    navigator.clipboard.writeText(email)
-        .then(() => {
-            alert('Email copied to clipboard!\nSend an email to say hi!');
-        })
-        .catch(err => {
-            console.error('Failed to copy: ', err);
-        });
+        const email = 'khaixuen.tang@.com';
+        navigator.clipboard.writeText(email)
+            .then(() => {
+                alert('Email copied to clipboard!\nSend an email to say hi!');
+            })
+            .catch(err => {
+                console.error('Failed to copy: ', err);
+            });
+    });
+
+    window.addEventListener('resize', function () {
+        if (window.innerHeight <= 500) {
+            this.document.getElementById('contact-section').classList.add('mt-60');
+        }
+        else {
+            this.document.getElementById('contact-section').classList.remove('mt-60');
+        }
+    });
 });
